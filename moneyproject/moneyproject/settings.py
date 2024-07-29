4516005c509070fb1c443a00b44fc2e1c582999b
soham1027 <sohamghayal02@gmail.com>
2024-07-29 18:48:59 +0530

    css  import

--- moneyproject/moneyproject/settings.py
+++ moneyproject/moneyproject/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moneyapp',
+    'sass_processor',
+    'widget_tweaks',
+
 ]
 
 MIDDLEWARE = [
@@ -120,10 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIR=[ os.path.join(BASE_DIR,'static') ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'staticfiles')
+STATIC_URL = 'static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'scssstaticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT = BASE_DIR/'media'
